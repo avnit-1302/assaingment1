f94b5e1556bf9672773f78a23032946235b1dff8
avnit-1302 <122270703+avnit-1302@users.noreply.github.com>
2024-09-15 13:45:26 +0200

    the quiz application

---
+++ android/app/src/main/kotlin/com/example/finished/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.finished
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/results_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary/questions_summary.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+    required this.onRestart,
+  });
+
+  final void Function() onRestart;
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> get summaryData {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
+
+  String getFeedback(int numCorrectQuestions, int numTotalQuestions) {
+    final double scorePercentage = (numCorrectQuestions / numTotalQuestions) * 100;
+
+    if (scorePercentage == 100) {
+      return 'Nah, you actually good, GG';
+    } else if (scorePercentage >= 75) {
+      return 'wæow, GRAPE!';
+    } else if (scorePercentage >= 50) {
+      return 'Bruv, this is not good, get yourself together';
+    } else if (scorePercentage >= 25) {
+      return 'You should be ashamed of yourself, go back to kindergarden';
+    } else {
+      return 'Keep practicing!';
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData
+        .where(
+          (data) => data['user_answer'] == data['correct_answer'],
+        )
+        .length;
+
+    final feedbackMessage = getFeedback(numCorrectQuestions, numTotalQuestions);
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              feedbackMessage,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                fontSize: 18,
+                fontStyle: FontStyle.italic,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(summaryData),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton.icon(
+              onPressed: onRestart,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: const Icon(Icons.refresh),
+              label: const Text('Restart Quiz!'),
+            )
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
